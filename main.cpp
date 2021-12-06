86aa653ecb97c5013ae1dd413c41dbe8edbd0982
Randmeer <das.randmeer@gmail.com>
2021-12-06 23:23:52 +0100

    level generation & block textures

--- main.cpp
+++ main.cpp
@@ -1,73 +1,11 @@
 #include <sstream>
 #include <SDL.h>
 #include <SDL_image.h>
+#include <SDL2_rotozoom.h>
 
-#define WINDOW_WIDTH 640
-#define WINDOW_HEIGHT 480
-
-void set_pixel(SDL_Renderer *rend, int x, int y) {
-    SDL_RenderDrawPoint(rend, x, y);
-}
-
-void draw_circle(SDL_Renderer *surface, int n_cx, int n_cy, int radius, Uint8 r, Uint8 g, Uint8 b, Uint8 a) {
-    SDL_SetRenderDrawColor(surface, r,g,b,a);
-
-    double error = (double)-radius;
-    double x = (double)radius - 0.5;
-    double y = (double)0.5;
-    double cx = n_cx - 0.5;
-    double cy = n_cy - 0.5;
-
-    while (x >= y) {
-        set_pixel(surface, (int)(cx + x), (int)(cy + y));
-        set_pixel(surface, (int)(cx + y), (int)(cy + x));
-
-        if (x != 0) {
-            set_pixel(surface, (int)(cx - x), (int)(cy + y));
-            set_pixel(surface, (int)(cx + y), (int)(cy - x));
-        }
-
-        if (y != 0) {
-            set_pixel(surface, (int)(cx + x), (int)(cy - y));
-            set_pixel(surface, (int)(cx - y), (int)(cy + x));
-        }
-
-        if (x != 0 && y != 0) {
-            set_pixel(surface, (int)(cx - x), (int)(cy - y));
-            set_pixel(surface, (int)(cx - y), (int)(cy - x));
-        }
-
-        error += y;
-        ++y;
-        error += y;
-
-        if (error >= 0) {
-            --x;
-            error -= x;
-            error -= x;
-        }
-    }
-    SDL_SetRenderDrawColor(surface, 255,255,255,255);
-}
-
-
-void draw_filled_circle(SDL_Renderer *surface, int cx, int cy, int radius, Uint8 r, Uint8 g, Uint8 b, Uint8 a) {
-    SDL_SetRenderDrawColor(surface, r,g,b,a);
-
-    for (double dy = 1; dy <= radius; dy += 1.0) {
-        double dx = floor(sqrt((2.0 * radius * dy) - (dy * dy)));
-        SDL_RenderDrawLine(surface, cx - dx, cy + dy - radius, cx + dx, cy + dy - radius);
-        SDL_RenderDrawLine(surface, cx - dx, cy - dy + radius, cx + dx, cy - dy + radius);
-    }
-    SDL_SetRenderDrawColor(surface, 255,255,255,255);
-}
-
-void draw_line(SDL_Renderer *surface, int x1, int y1, int x2, int y2, Uint8 r, Uint8 g, Uint8 b, Uint8 a) {
-    SDL_SetRenderDrawColor(surface, r,g,b,a);
-    SDL_RenderDrawLine(surface, x1, y1, x2, y2);
-    SDL_SetRenderDrawColor(surface, 255,255,255,255);
-}
-
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 450
+#define BACKGROUND_COLOR 255, 255, 255, 255
 
 void tick_delta(double& delta_time, Uint64& NOW, Uint64 LAST) {
     LAST = NOW;
@@ -75,24 +13,19 @@ void tick_delta(double& delta_time, Uint64& NOW, Uint64 LAST) {
     delta_time = (double)((NOW - LAST)*1000 / (double)SDL_GetPerformanceFrequency());
 }
 
-void tick_fpslimit(double delta_time, int delay_std) {
-    return;
-    if (delta_time >= delay_std) SDL_Delay(0);
-    else SDL_Delay(delay_std - delta_time);
+int randrange(int min, int max) {
+    return min+(std::rand()%(max-min+1));
 }
-
 
 int main(int argc, char ** argv)
 {
-    // variables
-
+    // event handling
     bool quit = false;
     SDL_Event event;
 
     // init SDL
-
     SDL_Init(SDL_INIT_VIDEO);
-    IMG_Init(IMG_INIT_JPG);
+    IMG_Init(IMG_INIT_PNG);
 
     // hints
     SDL_SetHint(SDL_HINT_RENDER_VSYNC, "1");
@@ -101,21 +34,12 @@ int main(int argc, char ** argv)
     // window and renderer
     SDL_Window * window = SDL_CreateWindow("Anima",SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
     SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-
-
-    // TIME STUFF
 
     // fps
     #define FPS_INTERVAL 1.0
     Uint32 fps_lasttime = SDL_GetTicks();
     Uint32 fps_current;
     Uint32 fps_frames = 0;
-
-    // fps limiter (currenly redundant because of vsync)
-    //#define FPS 60
-    //int delay_std = (1.0/FPS)*1000;
-    //int delay_actual = delay_std;
 
     // delta time
     Uint64 NOW = SDL_GetPerformanceCounter();
@@ -125,48 +49,94 @@ int main(int argc, char ** argv)
     // keystates
     const Uint8 *state = SDL_GetKeyboardState(NULL);
 
-    SDL_Surface* image = IMG_Load("/Users/emil/coding/c++/Anima/amogus.jpg");
-    if (image == NULL) {
-        return 5;
-    }
+    // all textures
+    SDL_Surface* textures = IMG_Load("../textures.png");
 
-    #define SIZE_PLAYER 50
+    // player
+    SDL_Rect playersrc;
+    SDL_Rect playerdest;
+    playersrc.x = 0;
+    playersrc.y = 0;
+    playersrc.w = 10;
+    playersrc.h = 20;
+    playerdest.x = 0;
+    playerdest.y = 0;
+    playerdest.w = 50;
+    playerdest.h = 100;
+    SDL_Surface* playersurf = SDL_CreateRGBSurface(0,10,20,32,0,0,0,0);
+    SDL_Texture* playertex = SDL_CreateTextureFromSurface(renderer, playersurf);
+    SDL_FreeSurface(playersurf);
+
+    // blocks
+    #define BLOCKSIZE 10
+    #define BLOCKSCALE 5
     SDL_Rect SrcR;
     SDL_Rect DestR;
-
     SrcR.x = 0;
     SrcR.y = 0;
-    SrcR.w = image->w;
-    SrcR.h = image->h;
-
-    DestR.x = 640 / 2 - SIZE_PLAYER / 2;
-    DestR.y = 480 / 2 - SIZE_PLAYER / 2;
-    DestR.w = SIZE_PLAYER;
-    DestR.h = SIZE_PLAYER;
-
-
-    //SDL_FillRect(image, NULL, SDL_MapRGB(image->format, 255, 0, 0));
-
-    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, image);
-    if (texture == NULL) {
-        return 5;
+    SrcR.w = BLOCKSIZE*BLOCKSCALE;
+    SrcR.h = BLOCKSIZE*BLOCKSCALE;
+    DestR.x = 0;
+    DestR.y = 0;
+    DestR.w = BLOCKSIZE*BLOCKSCALE;
+    DestR.h = BLOCKSIZE*BLOCKSCALE;
+    SDL_Texture *blocktextures[20];
+    SDL_Rect BlockDest;
+    BlockDest.x = 0;
+    BlockDest.y = 0;
+    SDL_Surface *blocksurf;
+    SDL_Texture *blocktex;
+    for (int i = 0; i < 20; i++) {
+        blocksurf = SDL_CreateRGBSurface(0,BLOCKSIZE,BLOCKSIZE,32, 0xff, 0xff00, 0xff0000, 0xff000000);
+        BlockDest.x = -BLOCKSIZE * (i % 10);
+        BlockDest.y = -BLOCKSIZE * ((i/10));
+        SDL_BlitSurface(textures, nullptr, blocksurf, &BlockDest);
+        blocksurf = rotozoomSurface(blocksurf, 0.0, BLOCKSCALE, 0);
+        blocktex = SDL_CreateTextureFromSurface(renderer, blocksurf);
+        blocktextures[i] = blocktex;
     }
-    SDL_FreeSurface(image);
+    SDL_FreeSurface(blocksurf);
 
-    // mouse position
+    // mouse stuff
     int mouseX = 0;
     int mouseY = 0;
+    Uint32 mouse_buttons;
 
+    // level generation stuff
+    #define LVL_W 16
+    #define LVL_H 10
+    int min = 0;
+    int max = 7;
+    int min_fl = 2;
+    int max_fl = 3;
+    int last_column_height = 0;
+    int column_height;
+    int blocks[LVL_W][LVL_H] = { 0 };
+    int flattener = randrange(min_fl, max_fl);
+    srand( time(NULL) );
+    for (int i = 0; i < LVL_W; i++) {
+        if (flattener < 0) {
+            min = last_column_height - 1;
+            max = last_column_height + 1;
+            if (min < 0) min = 0;
+            if (max > 7) max = 7;
+            column_height = randrange(min, max);
+            flattener = randrange(min_fl, max_fl);
+        }
+        else flattener--;
+        for (int j = column_height; j >=0; j--) {
+            blocks[i][j] = 1;
+        }
+        last_column_height = column_height;
+    }
 
     // game loop
     while (!quit)
     {
 
         // clock
-        //tick_fpslimit(delta_time, delay_std);
         SDL_PollEvent(&event);
         tick_delta(delta_time, NOW, LAST);
-
 
         // fps calculation
         fps_frames++;
@@ -183,6 +153,10 @@ int main(int argc, char ** argv)
         ss <<" FPS: " << fps_current;
         SDL_SetWindowTitle(window, ss.str().c_str());
 
+        // clear buffer
+        SDL_SetRenderDrawColor(renderer, BACKGROUND_COLOR);
+        SDL_RenderClear(renderer);
+
 
         // event handling
         switch (event.type)
@@ -190,54 +164,53 @@ int main(int argc, char ** argv)
             case SDL_QUIT:
                 quit = true;
                 break;
-            case SDL_KEYDOWN:
-                break;
-            case SDL_MOUSEBUTTONDOWN:
-                switch (event.button.button)
-                {
-                    case SDL_BUTTON_LEFT:
-                        SDL_ShowSimpleMessageBox(0, "Mouse", "Left button was pressed!", window);
-                        break;
-                    case SDL_BUTTON_RIGHT:
-                        SDL_ShowSimpleMessageBox(0, "Mouse", "Right button was pressed!", window);
-                        break;
-                    default:
-                        SDL_ShowSimpleMessageBox(0, "Mouse", "Some other button was pressed!", window);
-                        break;
-                }
-                break;
-            case SDL_MOUSEMOTION:
-                //one way to get the mouse pos
-                mouseX = event.motion.x;
-                mouseY = event.motion.y;
-                break;
         }
 
         // key states
-        if (state[SDL_SCANCODE_A]) DestR.x -= 1;
-        if (state[SDL_SCANCODE_D]) DestR.x += 1;
-        if (state[SDL_SCANCODE_W]) DestR.y -= 1;
-        if (state[SDL_SCANCODE_S]) DestR.y += 1;
+        if (state[SDL_SCANCODE_SPACE]) playerdest.y -= 1;
 
-        // another way to get the mouse pos
-        SDL_GetMouseState(&mouseX, &mouseY);
+        // mouse states & position
+        mouse_buttons = SDL_GetMouseState(&mouseX, &mouseY);
+        if (mouse_buttons & SDL_BUTTON_LMASK) ;// do something
 
-        SDL_RenderClear(renderer);
-        SDL_RenderCopy(renderer, texture, &SrcR, &DestR);
+        // level generation
+        // this works just because window size is hardcoded...
+        for (int i = 0; i < LVL_W; i++) {
+            DestR.x = i*BLOCKSIZE*BLOCKSCALE;
+            for (int j = LVL_H; j >= 0; j--) {
+                if (blocks[i][j]) {
+                    int x;
+                    int _1 = blocks[i-1][j-1];
+                    int _2 = blocks[i-1][j];
+                    int _3 = blocks[i-1][j+1];
+                    int _4 = blocks[i][j+1];
+                    int _5 = blocks[i+1][j+1];
+                    int _6 = blocks[i+1][j];
+                    int _7 = blocks[i+1][j-1];
+                    if (_3 && _4 && _5) x = 17;
+                    else if (_2 && _3 && _4 && _6) x = 16;
+                    else if (_2 && _4 && _5 && _6) x = 15;
+                    else if (_2 && _3 && _6) x = 11;
+                    else if (_2 && _5 && _6) x = 11;
+                    else if (_2 && _3 && _7 && !_4 && !_5 && !_6) x = 14;
+                    else if (_1 && _5 && _6 && !_2 && !_3 && !_4) x = 13;
+                    else if (_2 && _6) x = 11;
+                    else if (_2 && _7) x = 14;
+                    else if (_1 && _6) x = 13;
+                    else x = 0;
+                    DestR.y = WINDOW_HEIGHT-((j+1)*BLOCKSIZE*BLOCKSCALE);
+                    SDL_RenderCopy(renderer, blocktextures[x], &SrcR, &DestR);
+                }
+            }
+        }
 
-        // these lines have literally no purpose lol
-        draw_line(renderer, 0, 0, mouseX, mouseY, 255, 0, 0, 0);
-        draw_line(renderer, 0, WINDOW_HEIGHT, mouseX, mouseY, 255, 0, 0, 0);
-        draw_line(renderer, WINDOW_WIDTH, 0, mouseX, mouseY, 255, 0, 0, 0);
-        draw_line(renderer, WINDOW_WIDTH, WINDOW_HEIGHT, mouseX, mouseY, 255, 0, 0, 0);
+        // player
+        SDL_RenderCopy(renderer, playertex, &playersrc, &playerdest);
 
-        draw_circle(renderer, 100, 100, 50, 255, 0, 0, 255);
-        draw_filled_circle(renderer, 200, 200, 20, 255, 0, 0, 255);
         SDL_RenderPresent(renderer);
     }
 
     // cleanup SDL
-
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     IMG_Quit();
